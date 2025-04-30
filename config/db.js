09fed2a49094424b2177e66a2ac47347a2ca3877
Ahmadtareq34 <ahmadtareq100@gmail.com>
2025-04-30 23:31:09 +0300

    Added DB_PORT to MySQL connection

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASS || !pro
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
